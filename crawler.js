0e6aafdd0eb5c032d2d2f86ff78ef5337cdefb05
Mubashir <mubashir1728@gmail.com>
2023-09-15 00:48:50 +0530

    automated url fetching

--- crawler.js
+++ crawler.js
@@ -85,6 +85,7 @@ async function main() {
 
 main();
 */
+/*
 const puppeteer = require('puppeteer');
 
 async function getFacultyUrls(url) {
@@ -153,5 +154,76 @@ async function main() {
 }
 
 main();
+*/
+const puppeteer = require('puppeteer');
 
+async function getFacultyUrls(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Use page.evaluate to extract faculty URLs based on their href attribute
+    const facultyUrls = await page.evaluate(() => {
+        const facultyLinks = document.querySelectorAll('a'); // Select all <a> elements on the page
+        const urls = [];
+        for (const link of facultyLinks) {
+            const href = link.getAttribute('href');
+            if (href && href.startsWith('/facultynew/facultyprofile/id/')) {
+                // Filter links with href attribute starting with "/faculty/"
+                urls.push(href);
+            }
+        }
+        return urls;
+    });
+
+    await page.close(); // Close the page, but keep the browser instance open
+    return facultyUrls;
+}
+
+async function crawledProduct(url, browser) {
+    const page = await browser.newPage(); // Reuse the same browser instance
+    await page.goto(url);
+
+    const [nameEl] = await page.$x('//*[@id="main-title"]')
+    const name = (await nameEl.getProperty('textContent')).jsonValue();
+    
+    const [designationEl] = await page.$x('//*[@id="profile"]/div/div[2]/h3[1]');
+    const designation = (await designationEl.getProperty("textContent")).jsonValue();
+
+    const [deptEl] = await page.$x('//*[@id="profile"]/div/div[2]/h3[2]');
+    const department = (await deptEl.getProperty("textContent")).jsonValue();
+
+    const [emailEl] = await page.$x('//*[@id="profile"]/div/div[2]/p');
+    const email = (await emailEl.getProperty("textContent")).jsonValue();
+
+    console.log({name, department, designation, email});
+
+    await page.close(); // Close the page after scraping
+}
+
+async function main() {
+    const facultyListPageUrl = 'https://wsdc.nitw.ac.in/facultynew/dept/faculty_profiles/ece';
+
+    const browser = await puppeteer.launch();
+    const facultyUrls = await getFacultyUrls(facultyListPageUrl);
+    
+    if (facultyUrls.length > 0) {
+        console.log('Faculty URLs:');
+        facultyUrls.forEach((url, index) => {
+            console.log(`${index + 1}: ${url}`);
+        });
+
+        // Now, iterate through facultyUrls and call crawledProduct for each URL
+        for (const url of facultyUrls) {
+            await crawledProduct(`https://wsdc.nitw.ac.in${url}`, browser); // Pass the browser instance
+        }
+
+        // Close the browser instance when done
+        await browser.close();
+    } else {
+        console.log("No faculty URLs found on the page.");
+    }
+}
+
+main();
 
